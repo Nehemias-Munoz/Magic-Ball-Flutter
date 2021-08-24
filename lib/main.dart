25e38388e9c60b0ab5eebb0aa94563787355295b
Nehemias-Munoz <nehemiasmarvic.dev@gmail.com>
2021-08-24 10:00:44 -0400

    added functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,6 +18,33 @@ class BallPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Ask Me Anything'),
       ),
+      body: Ball(),
     );
+  }
+}
+
+class Ball extends StatefulWidget {
+  Ball({Key? key}) : super(key: key);
+
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Center(
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset('assets/images/ball$ballNumber.png'),
+      ),
+    ));
   }
 }
